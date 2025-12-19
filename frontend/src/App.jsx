99905a06548eb795771148df9d26d53b9df51d5e
sepupsHub <s3n0b22@gmail.com>
2025-12-19 16:47:20 +0100

    Create landing page and add routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "@/pages/Home"
+import Landing from "@/pages/Landing"
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,7 @@
+function Landing(){
+    return (<>
+        <h1>Welcome to the website</h1>
+    </>)
+};
+
+export default Landing;
