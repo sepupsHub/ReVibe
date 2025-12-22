194883572a42032ede5d52350f76ceaa30917534
sepupsHub <s3n0b22@gmail.com>
2025-12-22 16:19:07 +0100

    Add route protection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import ProtectedRoute from "@/routes/ProtectedRoute"
+
 import Home from "@/pages/Home"
 import Landing from "@/pages/Landing"
 
 function App() {
+    const isAuthenticated = false
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/home" element={<Home />} />
+
+        <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
+          <Route path="/app" element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
