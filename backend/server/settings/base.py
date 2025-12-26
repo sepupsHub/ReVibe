66799e3487be29f33a7ec7afbf7fd1aad66137eb
sepupsHub <s3n0b22@gmail.com>
2025-12-26 14:24:35 +0100

    Add spotify scopes parameter

--- backend/server/settings/base.py
+++ backend/server/settings/base.py
@@ -8,6 +8,11 @@ SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
 
+SPOTIFY_SCOPES = [
+    "user-read-private",
+    "playlist-read-private",
+]
+
 DEBUG = False
 
 ALLOWED_HOSTS = []
