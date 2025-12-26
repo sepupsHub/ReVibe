81c16c6469b788bb78ec34d1595157aed77d2d83
sepupsHub <s3n0b22@gmail.com>
2025-12-26 14:27:29 +0100

    import dotenv to read DJANGO_SETTINGS_MODULE paramter from .env

--- backend/manage.py
+++ backend/manage.py
@@ -2,11 +2,15 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings.dev')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE'
+        'DJANGO_SETTINGS_MODULE', 'server.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
