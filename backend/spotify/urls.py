747b199ac30d2ec5cb65171996587066bf6fdbfc
sepupsHub <s3n0b22@gmail.com>
2025-12-26 15:04:15 +0100

    Add /me endpoint to see logged users spotify data

--- backend/spotify/urls.py
+++ backend/spotify/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SpotifyLoginView, SpotifyCallbackView
+from .views import SpotifyLoginView, SpotifyCallbackView, SpotifyMeView
 
 urlpatterns = [
     path("login/", SpotifyLoginView.as_view()),
     path("callback/", SpotifyCallbackView.as_view()),
+    path("me/", SpotifyMeView.as_view())
 ]
