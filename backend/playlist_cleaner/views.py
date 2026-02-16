1e0632a4bfebd03fdca18da333682c4e9acf708b
sepupsHub <s3n0b22@gmail.com>
2026-02-16 09:28:54 +0100

    add temporary logic for fetching playlists

--- backend/playlist_cleaner/views.py
+++ backend/playlist_cleaner/views.py
@@ -2,10 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from ..spotify.services.tokens import get_valid_access_token
+from spotify.services.tokens import get_valid_access_token
+from spotify.services.client import spotify_get
 from .services.fetch_playlists import fetch_all_playlists
 
-class FetchPlaylists(APIView):
+class FetchPlaylistsView(APIView):
     def get(self, request):
         access_token = get_valid_access_token(request.session)
         
@@ -16,11 +17,17 @@ class FetchPlaylists(APIView):
             )
             
         try:
-            playlists = fetch_all_playlists(access_token)
+            playlists = spotify_get(access_token, "/me/playlists")
+            clean_playlists = []
+            for item in playlists["items"]:
+                clean_playlists.append({
+                    "id": item["id"],
+                    "name": item["name"]
+                })
         except Exception:
             return Response(
                 {"detail": "Failed to fetch Spotify profile"},
                 status=status.HTTP_502_BAD_GATEWAY,
             )
 
-        return Response(playlists, status=status.HTTP_200_OK)
+        return Response(clean_playlists, status=status.HTTP_200_OK)
