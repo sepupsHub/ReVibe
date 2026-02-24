c2b696097a12af9704f64735db721d9c65e81aea
sepupsHub <s3n0b22@gmail.com>
2026-02-24 14:47:43 +0100

    change format of data on views, fetch_all_songs now gets all the playlist songs

--- backend/playlist_cleaner/services/fetch_playlists.py
+++ backend/playlist_cleaner/services/fetch_playlists.py
@@ -2,44 +2,51 @@ from spotify.services.auth import spotify_get_or_raise
 
 def fetch_all_playlists(access_token):
     playlists = spotify_get_or_raise(access_token, "/me/playlists")
-    
     clean_playlists = []
+    data = dict()
     for item in playlists["items"]:
         clean_playlists.append({
             "id": item["id"],
             "name": item["name"]
         })
         
-    return clean_playlists
+    data["playlists"] = clean_playlists
+    data["total_playlists"] = len(playlists)
+    return data
 
 def fetch_all_songs(access_token):
     playlists = fetch_all_playlists(access_token)
     params = {
-        "fields": "items(track(name,id))",
+        "fields": "items(track(name,id)),next",
     }
     songs = []
+    data = dict()
     song_storage_dict = dict()
     total_songs = 0
     
-    for playlist in playlists:
-        new_songs = spotify_get_or_raise(
-            access_token,
-            f"/playlists/{playlist["id"]}/items",
-            params
-        )
-        for song in new_songs["items"]:
-            song_info = song["track"]
-            if not song_info: continue
-            if song_info["id"] in song_storage_dict:
-                position = song_storage_dict[song_info["id"]]
-                song_info = songs[position]
-                song_info["playlists"].append(playlist)
-                continue
+    for playlist in playlists["playlists"]:
+        path = f"/playlists/{playlist["id"]}/items"
+        new_songs = spotify_get_or_raise(access_token, path, params)
+        while new_songs:
             
-            song_info["playlists"] = [playlist]
-            songs.append(song_info)
-            song_storage_dict[song_info["id"]] = total_songs
-            total_songs += 1
+            for song in new_songs["items"]:
+                song_info = song["track"]
+                if not song_info: continue
+                if song_info["id"] in song_storage_dict:
+                    position = song_storage_dict[song_info["id"]]
+                    song_info = songs[position]
+                    song_info["playlists"].append(playlist)
+                    continue
+                
+                song_info["playlists"] = [playlist]
+                songs.append(song_info)
+                song_storage_dict[song_info["id"]] = total_songs
+                total_songs += 1
+                
+            if not new_songs["next"]: break
+            path = new_songs["next"][26:]
+            new_songs = spotify_get_or_raise(access_token, path, params)
             
-    # songs.append(total_songs)
-    return songs
+    data["songs"] = songs
+    data["total_songs"] = total_songs
+    return data
