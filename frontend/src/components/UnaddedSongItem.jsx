cc43ea70d5d7008c775a2beb5cc3d87658081df2
sepupsHub <s3n0b22@gmail.com>
2026-03-16 09:48:11 +0100

    Add image rendering to UnaddedSongItem component

--- frontend/src/components/UnaddedSongItem.jsx
+++ frontend/src/components/UnaddedSongItem.jsx
@@ -3,9 +3,11 @@ function SongItem({ song }) {
     const artists = Array.isArray(song?.artists)
         ? song.artists.map((artist) => artist?.name ?? artist).join(", ")
         : song?.artist ?? "Unknown artist"
+    const imageSource = song.album.images[2].url
 
     return (
         <div>
+            <img src={imageSource} alt="" />
             <p>{title}</p>
             <small>{artists}</small>
         </div>
